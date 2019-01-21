a03087ea2facc12ad2df48622880fb01ed102d05
Kevin Ruggiero <kevinbruggiero@gmail.com>
2019-01-21 21:12:59 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -27,9 +27,11 @@ def end_game(card_total)
 end
 
 def initial_round
-  sum = rand(1..11) + rand(1..11)
-  display_card_total(sum)
-
+  card_one = deal_card
+  card_two = deal_card
+  card_total = card_one + card_two
+  display_card_total(card_total)
+  return card_total
 
   # code #initial_round here
 end
